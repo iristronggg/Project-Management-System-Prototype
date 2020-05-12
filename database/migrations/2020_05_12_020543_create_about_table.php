085ff5c405e66204c8d86ba291fb4e086f561d1c
iristronggg <b06705033@ntu.edu.tw>
2020-05-12 10:36:03 +0800

    Add migrations for create tables

--- database/migrations/2020_05_12_020543_create_about_table.php
+++ database/migrations/2020_05_12_020543_create_about_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateAboutTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,16 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+        Schema::create('about', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('content');
+            $table->string('image');
         });
+        /*
+        Schema::create('about', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });*/
     }
 
     /**
@@ -31,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('about');
     }
 }
